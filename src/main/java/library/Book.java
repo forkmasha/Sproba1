cdc1db5fc567f9a559861e1bcaca6aa0acacf9e6
Asus <forkmasha100@gmail.com>
2022-05-17 15:58:27 +0300

    first commit

---
+++ src/main/java/library/Book.java
@@ -0,0 +1,64 @@
+package library;
+
+public class Book extends Item{
+    private int pages;
+    private String authorName;
+    private String publishingData;
+    private String bookName;
+
+    public Book(int pages, String authorName, String publishingData, String bookName) {
+        this.pages = pages;
+        this.authorName = authorName;
+        this.publishingData = publishingData;
+        this.bookName = bookName;
+    }
+
+    public Book() {
+        this.pages =0;
+        this.authorName ="default";
+        this.publishingData ="default";
+         this.bookName ="default";
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setPublishingData(String publishingData) {
+        this.publishingData = publishingData;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getPublishingData() {
+        return publishingData;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "pages=" + pages +
+                ", authorName='" + authorName + '\'' +
+                ", publishingData='" + publishingData + '\'' +
+                ", bookName='" + bookName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/library/Item.java
@@ -0,0 +1,48 @@
+package library;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Item {
+    public static void main(String args[]) {
+
+        List<Book> listBook = booksFromfile();
+
+        System.out.println(listBook);
+        System.out.println("*****************************888888");
+
+        listBook.get(0).setAuthorName("myAuthorName");
+        listBook.get(0).setBookName("myBookName");
+        listBook.get(0).setPublishingData("myPublishingData");
+
+        System.out.println(listBook);
+
+        booksToFile(listBook);
+    }
+    private static List<Book> booksFromfile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Book> listBook = null;
+        try {
+            listBook = objectMapper
+                    .readValue(Paths.get("book.json").toFile(),
+                            objectMapper.getTypeFactory().constructCollectionType(List.class, Book.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listBook;
+    }
+
+    private static void booksToFile(List<Book> books) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            objectMapper.writeValue(new File("book.json"), books);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
